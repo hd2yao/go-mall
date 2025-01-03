4b039fbead10b18b2121e22fe9a36aad644ada29
hd2yao <1054290355@qq.com>
2025-01-03 17:02:06 +0800

    feat(logger): 增加日志调用者信息

--- common/logger/logger.go
+++ common/logger/logger.go
@@ -3,6 +3,8 @@ package logger
 import (
     "context"
     "fmt"
+    "path"
+    "runtime"
 
     "go.uber.org/zap"
     "go.uber.org/zap/zapcore"
@@ -57,6 +59,13 @@ func (l *logger) log(lvl zapcore.Level, msg string, kv ...interface{}) {
     if len(kv)%2 != 0 {
         kv = append(kv, "unknown")
     }
+    // 日志信息中增加追踪参数
+    kv = append(kv, "traceId", l.traceId, "spanId", l.spanId, "pSpanId", l.pSpanId)
+    // 增加日志调用者信息，方便查日志时定位程序位置
+    funcName, file, line := l.getLoggerCallerInfo()
+    kv = append(kv, "funcName", funcName, "file", file, "line", line)
+
+    // 将 kv 转换成 zap.Field 切片
     fields := make([]zap.Field, 0, len(kv)/2)
     for i := 0; i < len(kv); i += 2 {
         k := fmt.Sprintf("%v", kv[i])
@@ -66,4 +75,20 @@ func (l *logger) log(lvl zapcore.Level, msg string, kv ...interface{}) {
     // 调用 Check 方法，判断这个日志级别是否能写入日志
     ce := l._logger.Check(lvl, msg)
     ce.Write(fields...)
+}
+
+// getLoggerCallerInfo 获取调用日志记录器的方法的函数名、文件名和行号
+func (l *logger) getLoggerCallerInfo() (funcName, file string, line int) {
+    // runtime.Caller(skip int)：skip 参数决定了要跳过的堆栈帧数量。
+    // 0：当前函数（getLoggerCallerInfo）
+    // 1：调用 getLoggerCallerInfo 的函数 function (l *logger) log(lvl zapcore.Level, msg string, kv ...interface{})
+    // 2：再上一层的调用者 function (l *logger) Info(mag string, kv ...interface{})
+    // 3：通常是用户直接调用日志记录器的方法，例如 logger.Info()
+    pc, file, line, ok := runtime.Caller(3)
+    if !ok {
+        return "", "", 0
+    }
+    file = path.Base(file)
+    funcName = runtime.FuncForPC(pc).Name()
+    return funcName, file, line
 }
