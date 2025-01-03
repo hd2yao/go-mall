4a125fa8eb22e9fd0266b144a2e95a4b2e55f515
hd2yao <1054290355@qq.com>
2025-01-03 16:45:04 +0800

    feat(infrastructure): 构建日志追踪信息的中间件

--- common/middleware/infrastructure.go
+++ common/middleware/infrastructure.go
@@ -1,1 +1,28 @@
 package middleware
+
+import (
+    "github.com/gin-gonic/gin"
+
+    "github.com/hd2yao/go-mall/common/util"
+)
+
+// 存放项目运行需要的基础中间件
+
+func StartTrace() gin.HandlerFunc {
+    return func(c *gin.Context) {
+        traceId := c.Request.Header.Get("traceid")
+        pSpanId := c.Request.Header.Get("spanid")
+        spanId := util.GenerateSpanID(c.Request.RemoteAddr)
+
+        // 如果 traceId 为空，证明是链路的发端，把它设置成此次的 spanId
+        if traceId == "" {
+            // trace 标识整个请求的链路，span 标识链路中的不同服务
+            traceId = spanId
+        }
+
+        c.Set("traceId", traceId)
+        c.Set("spanId", spanId)
+        c.Set("pSpanId", pSpanId)
+        c.Next()
+    }
+}
