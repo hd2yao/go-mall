1b01d2c8b72e531e92ea76a42da7d61ffa19a754
hd2yao <1054290355@qq.com>
2025-01-06 18:07:51 +0800

    feat(error): 包装错误

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+    "errors"
     "net/http"
 
     "github.com/gin-gonic/gin"
 
+    "github.com/hd2yao/go-mall/common/errcode"
     "github.com/hd2yao/go-mall/common/logger"
     "github.com/hd2yao/go-mall/common/middleware"
     "github.com/hd2yao/go-mall/config"
@@ -55,6 +57,26 @@ func main() {
             "status": "ok",
             "data":   a,
         })
+    })
+
+    g.GET("/customized-error-test", func(c *gin.Context) {
+
+        // 使用 Wrap 包装原因error 生成 项目error
+        err := errors.New("a dao error")
+        appErr := errcode.Wrap("包装错误", err)
+        bAppErr := errcode.Wrap("再包装错误", appErr)
+        logger.New(c).Error("记录错误", "err", bAppErr)
+
+        // 预定义的ErrServer, 给其追加错误原因的error
+        err = errors.New("a domain error")
+        apiErr := errcode.ErrServer.WithCause(err)
+        logger.New(c).Error("API执行中出现错误", "err", apiErr)
+
+        c.JSON(apiErr.HttpStatusCode(), gin.H{
+            "code": apiErr.Code(),
+            "msg":  apiErr.Msg(),
+        })
+
     })
 
     g.Run()
