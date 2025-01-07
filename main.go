c60c201cb786d6c92eca5a2f74916bbcddfb97d0
hd2yao <1054290355@qq.com>
2025-01-07 14:59:16 +0800

    style: Normalize line endings to LF

--- main.go
+++ main.go
@@ -1,19 +1,19 @@
 package main
 
 import (
-    "github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin"
 
-    "github.com/hd2yao/go-mall/api/router"
-    "github.com/hd2yao/go-mall/common/enum"
-    "github.com/hd2yao/go-mall/config"
+	"github.com/hd2yao/go-mall/api/router"
+	"github.com/hd2yao/go-mall/common/enum"
+	"github.com/hd2yao/go-mall/config"
 )
 
 func main() {
-    if config.App.Env == enum.ModeProd {
-        gin.SetMode(gin.ReleaseMode)
-    }
+	if config.App.Env == enum.ModeProd {
+		gin.SetMode(gin.ReleaseMode)
+	}
 
-    g := gin.New()
-    router.RegisterRoutes(g)
-    g.Run(":8080")
+	g := gin.New()
+	router.RegisterRoutes(g)
+	g.Run(":8080")
 }
